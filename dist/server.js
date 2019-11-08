6065c323536b1d44edb9f8606d09c15cc60ab500
Jason Poindexter <poindexter.json@gmail.com>
2019-11-08 13:33:07 -0800

    Implement basic socetio namespaces and http session

--- dist/server.js
+++ dist/server.js
@@ -1,26 +1,75 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const bluebird_1 = __importDefault(require("bluebird"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
 const http_1 = require("http");
+const morgan_1 = __importDefault(require("morgan"));
+const redis_1 = __importDefault(require("redis"));
 const socket_io_1 = __importDefault(require("socket.io"));
-const cors_1 = __importDefault(require("cors"));
+const v4_1 = __importDefault(require("uuid/v4"));
+// Redis Config
+bluebird_1.default.promisifyAll(redis_1.default.RedisClient.prototype);
+bluebird_1.default.promisifyAll(redis_1.default.Multi.prototype);
+const redisClient = redis_1.default.createClient();
+redisClient.on("error", err => {
+    console.log("Error " + err);
+});
+// Express Config
 const app = express_1.default();
-const port = 8081; // default port to listen
+const port = 8081; // default express port to listen
 app.set("port", process.env.PORT || 8081);
+app.use(morgan_1.default(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'));
+app.use(body_parser_1.default.json());
 app.use(cors_1.default({ origin: "http://localhost:8080", credentials: true }));
+// SocketIo Config
 const http = new http_1.Server(app);
 const io = socket_io_1.default(http);
-io.on("connect", (socket) => {
-    console.log("a user connected");
-});
-// define a route handler for the default home page
+// Handle creating a new session
 app.get("/", (req, res) => {
-    console.log('app.get("/")');
-    res.send("Hello world!");
+    res.send(v4_1.default());
 });
+// Handle joining a session
+app.post("/session/:session", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const sessionId = req.params.session;
+    try {
+        // @ts-ignore
+        const session = yield redisClient.getAsync(sessionId);
+        if (session !== null) {
+            console.log(`found current session: ${sessionId}`);
+            res.send(sessionId);
+        }
+        else {
+            console.log(`creating new session: ${sessionId}`);
+            // @ts-ignore
+            yield redisClient.setAsync(sessionId, "");
+            const namespace = io.of(sessionId);
+            namespace.on("connection", (socket) => {
+                console.log("user connected to session: ", sessionId);
+            });
+            // Send something to all
+            namespace.emit(sessionId, sessionId);
+            res.send(sessionId);
+        }
+    }
+    catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+}));
 http.listen(port, () => {
     // tslint:disable-next-line:no-console
     console.log(`server started at http://localhost:${port}`);
--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,70 @@
+import bluebird from "bluebird";
+import bodyParser from "body-parser";
+import cors from "cors";
 import express from "express";
 import { Server } from "http";
-import socketio from "socket.io";
-import cors from "cors";
+import morgan from "morgan";
+import redis from "redis";
+import socketio, { Socket } from "socket.io";
+import uuid from "uuid/v4";
 
+// Redis Config
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+const redisClient = redis.createClient();
+redisClient.on("error", err => {
+  console.log("Error " + err);
+});
+
+// Express Config
 const app = express();
-const port = 8081; // default port to listen
+const port = 8081; // default express port to listen
 app.set("port", process.env.PORT || 8081);
+app.use(
+  morgan(
+    ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'
+  )
+);
+app.use(bodyParser.json());
 app.use(cors({ origin: "http://localhost:8080", credentials: true }));
+
+// SocketIo Config
 const http = new Server(app);
 const io = socketio(http);
 
-io.on("connect", (socket: any) => {
-  console.log("a user connected");
+// Handle creating a new session
+app.get("/", (req, res) => {
+  res.send(uuid());
 });
 
-// define a route handler for the default home page
-app.get("/", (req, res) => {
-  console.log('app.get("/")');
-  res.send("Hello world!");
+// Handle joining a session
+app.post("/session/:session", async (req, res) => {
+  const sessionId = req.params.session;
+  try {
+    // @ts-ignore
+    const session = await redisClient.getAsync(sessionId);
+    if (session !== null) {
+      console.log(`found current session: ${sessionId}`);
+      res.send(sessionId);
+    } else {
+      console.log(`creating new session: ${sessionId}`);
+      // @ts-ignore
+      await redisClient.setAsync(sessionId, "");
+      const namespace = io.of(sessionId);
+      namespace.on("connection", (socket: Socket) => {
+        console.log("user connected to session: ", sessionId);
+      });
+      // Send something to all
+      namespace.emit(sessionId, sessionId);
+      res.send(sessionId);
+    }
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 });
 
 http.listen(port, () => {
   // tslint:disable-next-line:no-console
   console.log(`server started at http://localhost:${port}`);
 });
-
